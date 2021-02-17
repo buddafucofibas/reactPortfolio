d43d5953485c789ad6bbfaf594ee3a66d586d653
Dorian Edwards <de.pharmd2018@gmail.com>
2021-02-17 08:59:32 -0500

    added images and about component

--- src/Components/Sections/About.js
+++ src/Components/Sections/About.js
@@ -1,5 +1,20 @@
+import { concat } from '../../Utility'
+import styles from './About.module.css'
+
 const about = () => {
-  return <div className='page'>hey from about component</div>
+  return (
+    <div className={concat(styles.flex_center, 'page')}>
+      <div className={styles.image}></div>
+      <div className={styles.lead}>
+        <p>
+          i am a pharmacist by trade and a coder by heart. i'm driven, passionate, dedicated and
+          most importantly, a life-long learner. coding started as a hobby, a leftover from an
+          earlier foray into tech, and has been a endeavor that has come to fill every second of my
+          free time.
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default about
