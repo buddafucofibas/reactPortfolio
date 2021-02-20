def714ce68b73ede4c7b82393f057306f69a27ce
Dorian Edwards <de.pharmd2018@gmail.com>
2021-02-20 10:31:31 -0500

    changed class attribute to className for icons

--- src/Components/Sections/Contact.js
+++ src/Components/Sections/Contact.js
@@ -9,20 +9,20 @@ const contact = () => {
       <div className={concat(styles.contactWrapper)}>
         <div className={concat(styles.navRow, styles.center)}>
           <a href='https://twitter.com/PharmDTechie' target='_blank'>
-            <i class='fab fa-twitter'></i>
+            <i className='fab fa-twitter'></i>
           </a>
         </div>
         <div className={concat(styles.navRow, styles.spread)}>
           <a href='https://github.com/buddafucofibas' target='_blank'>
-            <i class='fab fa-github-alt'></i>
+            <i className='fab fa-github-alt'></i>
           </a>
           <a href='https://www.linkedin.com/in/dorian-edwards-442a49181/' target='_blank'>
-            <i class='fab fa-linkedin-in'></i>
+            <i className='fab fa-linkedin-in'></i>
           </a>
         </div>
         <div className={concat(styles.navRow, styles.center)}>
           <a href='mailto:dorian.m.edwards@gmail.com?subject=When+can+you+start?' target='_blank'>
-            <i class='fas fa-envelope-open-text'></i>
+            <i className='fas fa-envelope-open-text'></i>
           </a>
         </div>
       </div>
