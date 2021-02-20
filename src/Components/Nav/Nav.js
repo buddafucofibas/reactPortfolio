4c6207dfcd23f89703d92f2d5e4bc37368d18bde
Dorian Edwards <de.pharmd2018@gmail.com>
2021-02-20 10:48:10 -0500

    added smooth scrolling to pages

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,25 +1,43 @@
 import styles from './Nav.module.css'
 import { concat } from '../../Utility'
+import React, { Component } from 'react'
 
-const Nav = () => {
-  return (
-    <div className={styles.navWrapper}>
-      <ul className={styles.nav}>
-        <li className={concat(styles.navLink, 'rando1')}>
-          <a href='#home'>home</a>
-        </li>
-        <li className={concat(styles.navLink, 'rando2')}>
-          <a href='#about'>about</a>
-        </li>
-        <li className={concat(styles.navLink, 'rando3')}>
-          <a href='#projects'>projects</a>
-        </li>
-        <li className={concat(styles.navLink, 'rando4')}>
-          <a href='#contact'>contact</a>
-        </li>
-      </ul>
-    </div>
-  )
+class Nav extends Component {
+  handleNav = event => {
+    event.preventDefault()
+    let destination = event.target.id
+    let page = document.querySelector(destination)
+    page.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  render() {
+    return (
+      <div className={styles.navWrapper}>
+        <ul className={styles.nav}>
+          <li className={concat(styles.navLink, 'rando1')}>
+            <a id='#home' onClick={this.handleNav}>
+              home
+            </a>
+          </li>
+          <li className={concat(styles.navLink, 'rando2')}>
+            <a id='#about' onClick={this.handleNav}>
+              about
+            </a>
+          </li>
+          <li className={concat(styles.navLink, 'rando3')}>
+            <a id='#projects' onClick={this.handleNav}>
+              projects
+            </a>
+          </li>
+          <li className={concat(styles.navLink, 'rando4')}>
+            <a id='#contact' onClick={this.handleNav}>
+              contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Nav
