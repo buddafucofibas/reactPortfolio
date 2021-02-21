6592ac952c43798b154d77fbf1a2cd9f80fcb5b4
Dorian Edwards <de.pharmd2018@gmail.com>
2021-02-21 12:51:37 -0500

    changed Wrapper component to class component

--- src/Components/Wrapper/Wrapper.js
+++ src/Components/Wrapper/Wrapper.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react'
 import styles from './Wrapper.module.css'
 import Home from '../Sections/Home'
 import About from '../Sections/About'
@@ -5,20 +6,22 @@ import Projects from '../Sections/Projects'
 import Contact from '../Sections/Contact'
 import Nav from '../Nav/Nav'
 
-const Wrapper = () => {
-  return (
-    <div className={styles.pageWrapper}>
-      <div className={styles.navDisplayPane}>
-        <Nav />
+class Wrapper extends Component {
+  render() {
+    return (
+      <div className={styles.pageWrapper}>
+        <div className={styles.navDisplayPane}>
+          <Nav />
+        </div>
+        <div className={styles.contentDisplayPane}>
+          <Home />
+          <About />
+          <Projects />
+          <Contact />
+        </div>
       </div>
-      <div className={styles.contentDisplayPane}>
-        <Home />
-        <About />
-        <Projects />
-        <Contact />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Wrapper
